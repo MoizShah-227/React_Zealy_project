fbe490ddf28ffdb32e0d2361efa9971f2e36f7cd
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-08-02 10:54:27 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+   <div className='components-wrapper '>
+     <Navbar/>
+   </div>
+  );
+}
+
+export default App;
